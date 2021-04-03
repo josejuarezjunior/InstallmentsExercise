b374351a3e702dec9ce06c0cb0f94ae893202add
Jose Juarez <juarez.junior@outlook.com>
2021-04-03 09:33:53 -0300

    Updating 'ContractServic'.

--- Course/Services/ContractService.cs
+++ Course/Services/ContractService.cs
@@ -17,8 +17,8 @@ namespace Course.Services
             for(int i = 1; i <= months; i++)
             {
                 DateTime date = contract.Date.AddMonths(i);
-                double updateQuota = basicQuota + _onlinePaymentService.Interest(basicQuota, i);
-                double fullQuota = updateQuota + _onlinePaymentService.PaymentFee(updateQuota);
+                double updatedQuota = basicQuota + _onlinePaymentService.Interest(basicQuota, i);
+                double fullQuota = updatedQuota + _onlinePaymentService.PaymentFee(updatedQuota);
                 contract.AddInstallment(new Installment(date, fullQuota));
             }
 
